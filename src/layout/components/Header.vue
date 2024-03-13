4ac28ed46a2b962b7ecd828eb0181a23070b97de
whiteday26 <whiteday26@gmail.com>
2024-03-13 17:26:02 +0900

    0313

--- src/layout/components/Header.vue
+++ src/layout/components/Header.vue
@@ -2,10 +2,11 @@
     <header>
         <div class="topmenu">
             <div class="contentbox">
-                <div class="logo"></div>
+                <div class="logo">
                     <button>
                         <img src="/images/logo.png" alt="이지스퍼블리싱 로고" />
                     </button>
+                </div>
                 <div class="system">
                     <button class="login">로그인</button>
                     <button class="member">회원가입</button>
@@ -13,7 +14,15 @@
             </div>
         </div>
         <nav>
-            <div class="contentbox"></div>
+            <div class="contentbox">
+                <ul>
+                    <li><button>도서 소개</button></li>
+                    <li><button>자료실</button></li>
+                    <li><button>동영상 강의</button></li>
+                    <li><button>교재 샘플</button></li>
+                    <li><button>회사 소개</button></li>
+                </ul>
+            </div>
         </nav>
     </header>
 </template>
